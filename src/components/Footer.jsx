e0ada74c6de31a1486d5ebfd2bf4ee6a06bbf8c7
Daniel <danielduyilemi@gmail.com>
2022-11-25 23:11:22 +0100

    Fixed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import {Facebook, Twitter, Instagram, Youtube} from 'react-feather'
+import {
+	Facebook,
+	Twitter,
+	Instagram,
+	Youtube,
+	Plus,
+	Minus,
+} from 'react-feather';
+import { AppContext } from '../Context';
 
 export default function Footer() {
+	const { isDesktop } = useContext(AppContext);
+
+	//STATES
+	const [marketsOpen, setMarketsOpen] = useState(false);
+	const [resourcesOpen, setResourcesOpen] = useState(false);
+
 	return (
 		<footer className="py-8 border-t-gray-100 border-t-[1px] mt-20 text-black">
 			<div>
@@ -11,14 +25,43 @@ export default function Footer() {
 						<div className="fl-itcn logo- text-[1.5rem] md:text-[2rem] font-bold text-black uppercase">
 							<Link to="/">JohnBons</Link>
 						</div>
-						<div className='pt-12'>
+						<div className="pt-12">
 							<div>Need Support?</div>
-							<div className='text-primary-brown font-semibold'>Support@johnbons.com</div>
+							<div className="text-primary-brown font-semibold">
+								Support@johnbons.com
+							</div>
 						</div>
 					</div>
 					<div>
-						<div className="py-4 font-semibold">Our Markets</div>
-						<ul className="flex gap-12">
+						<div
+							className={`py-4 font-semibold ${
+								isDesktop ? '' : 'flex justify-between'
+							}`}
+						>
+							<div>Our Markets</div>{' '}
+							{marketsOpen ? (
+								<Minus
+									onClick={() => {
+										setMarketsOpen(false);
+									}}
+								/>
+							) : (
+								<Plus
+									className=""
+									onClick={() => {
+										setMarketsOpen(true);
+										setResourcesOpen(false);
+									}}
+								/>
+							)}{' '}
+						</div>
+						<ul
+							className={`${
+								isDesktop ? 'flex' : ''
+							} gap-12 transition-[height] ${
+								marketsOpen ? 'h-auto overflow-visible' : 'h-0 overflow-hidden'
+							}`}
+						>
 							<div>
 								<li className="flex-1 py-2">Atlanta</li>
 								<li className="flex-1 py-2">Cincinnati</li>
@@ -45,8 +88,33 @@ export default function Footer() {
 						</ul>
 					</div>
 					<div>
-						<div className="py-4 font-semibold">Resources</div>
-						<ul className="">
+						<div
+							className={`py-4 font-semibold ${
+								isDesktop ? '' : 'flex justify-between'
+							}`}
+						>
+							<div>Resources</div>{' '}
+							{resourcesOpen ? (
+								<Minus onClick={() => setResourcesOpen(false)} />
+							) : (
+								<Plus
+									className=""
+									onClick={() => {
+										setResourcesOpen(true);
+										setMarketsOpen(false);
+									}}
+								/>
+							)}{' '}
+						</div>
+						<ul
+							className={`${
+								isDesktop ? 'flex' : ''
+							} gap-12 transition-[height] ${
+								resourcesOpen
+									? 'h-auto overflow-visible'
+									: 'h-0 overflow-hidden'
+							}`}
+						>
 							<li className="flex-1 py-2">Partnership inquiries</li>
 							<li className="flex-1 py-2">Media Inquires</li>
 							<li className="flex-1 py-2">Search Homes</li>
@@ -59,36 +127,51 @@ export default function Footer() {
 						</ul>
 					</div>
 					<div>
-						<div className="py-4 font-semibold">Connect</div>
+						<div className="py-4 font-semibold">
+							<div>Connect</div>{' '}
+						</div>
 						<ul className="">
 							<li className="flex-1 py-2 flex items-center gap-4">
-								<div className='w-12 h-12 grid place-items-center rounded-full bg-black' size={16}>
-									<Facebook className='text-white'/>
+								<div
+									className="w-12 h-12 grid place-items-center rounded-full bg-black"
+									size={16}
+								>
+									<Facebook className="text-white" />
 								</div>
-								<div className='font-bold'>Facebook</div>
+								<div className="font-bold">Facebook</div>
 							</li>
 							<li className="flex-1 py-2 flex items-center gap-4">
-								<div className='w-12 h-12 grid place-items-center rounded-full bg-black' size={16}>
-									<Twitter className='text-white'/>
+								<div
+									className="w-12 h-12 grid place-items-center rounded-full bg-black"
+									size={16}
+								>
+									<Twitter className="text-white" />
 								</div>
-								<div className='font-bold'>Twitter</div>
+								<div className="font-bold">Twitter</div>
 							</li>
 							<li className="flex-1 py-2 flex items-center gap-4">
-								<div className='w-12 h-12 grid place-items-center rounded-full bg-black' size={16}>
-									<Instagram className='text-white'/>
+								<div
+									className="w-12 h-12 grid place-items-center rounded-full bg-black"
+									size={16}
+								>
+									<Instagram className="text-white" />
 								</div>
-								<div className='font-bold'>Instagram</div>
+								<div className="font-bold">Instagram</div>
 							</li>
 							<li className="flex-1 py-2 flex items-center gap-4">
-								<div className='w-12 h-12 grid place-items-center rounded-full bg-black' size={16}>
-									<Youtube className='text-white'/>
+								<div
+									className="w-12 h-12 grid place-items-center rounded-full bg-black"
+									size={16}
+								>
+									<Youtube className="text-white" />
 								</div>
-								<div className='font-bold'>Youtube</div>
+								<div className="font-bold">Youtube</div>
 							</li>
 						</ul>
 					</div>
 				</div>
 			</div>
+			{/* <div className='hover:h-12 h-8 transition-[height] w-12 bg-black duration-[1s]'></div> */}
 		</footer>
 	);
 }
