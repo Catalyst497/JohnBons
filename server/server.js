7c5ea5bcda6f54424bca4e60733b321e7755fb3c
Daniel <danielduyilemi@gmail.com>
2022-12-23 19:37:05 +0100

    Change port to env

--- server/server.js
+++ server/server.js
@@ -81,7 +81,7 @@ app.get('*', (req, res) => {
 	res.sendFile(filePath);
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
 	console.log(`App running on PORT: ${PORT}`);
 });
