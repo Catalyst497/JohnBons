8c07f644a419033dd727bdabec51727c9ac61302
Daniel <danielduyilemi@gmail.com>
2022-12-24 00:23:32 +0100

    Hai.

--- client/src/pages/ProductPage.jsx
+++ client/src/pages/ProductPage.jsx
@@ -4,25 +4,40 @@ import { useParams } from 'react-router-dom';
 
 export default function ProductPage() {
 	const searchResults = JSON.parse(localStorage.getItem('searchResults'));
+	console.log(searchResults);
 	const params = useParams();
 	const { property_id } = params;
 	const property = searchResults.find(
 		(item) => item?.property_id == property_id
 	);
-	const { photo } = property;
+	const { photo, address } = property;
 	return (
-		<div className="ProductPage">
+		<div className="ProductPage mt-[-2rem] ">
 			<div className="Container w-[90%] max-w-[900px] mx-auto">
 				<div className="relative bg-gray-100">
 					<div>
-						<img src={photo} className="md:w-[85%] w-full mx-auto" alt="" />
+						<img
+							src={photo}
+							className="md:h-[75vh] md:max-w-[90%] mx-auto"
+							alt=""
+						/>
 					</div>
-					<div className="absolute bottom-0 right-4 translate-y-[90%] bg-white">
+					<div className="absolute bottom-0 right-4 translate-y-[80%] p-4 bg-white">
+						<div className="flex items-center gap-4 justify-end">
+							<div>
+								<Heart />
+							</div>
+							<div>
+								<Share2 />
+							</div>
+						</div>
 						<div>
-							<div><Heart /></div>
-							<div><Share2 /></div>
+							<div className="font-bold text-[2rem]"></div>
 						</div>
 					</div>
+				</div>
+				<div>
+					<div className="font-bold text-[1.5rem] md:text-[2.5rem] text-black mt-4">{address}</div>
 				</div>
 			</div>
 		</div>
